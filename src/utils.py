5c743fef3ba588cbd74ec4c019ae45c13483a363
Bayzid Parvez <care4nishan@gmail.com>
2026-04-29 14:04:09 +0600

    Add multiply function to utils.py

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,13 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
-# Add test code for calculator functions
+# Test code for calculator functions
 print(add(10, 5))
 print(subtract(10, 5))
+
+
+def multiply(a, b):
+    return a * b
+
+# test code
+print(f"Multiplication of 10 and 5 is: {multiply(10, 5)}")
